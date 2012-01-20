28a06648cc19f18ac02145787121ca47709f549c
Thiago Franco de Moraes <totonixsame@gmail.com>
2012-01-20 11:19:25 -0200

    Trying to find the vertices in staircase artifacts

--- ca_smoothing.py
+++ ca_smoothing.py
@@ -1,17 +1,53 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+import itertools
+import numpy as np
 import sys
 import vtk
 
-def find_staircase_artifacts(pd):
+def find_staircase_artifacts(pd, stack_orientation=(0, 0, 1), T=0.7):
     """
     This function is used to find vertices at staircase artifacts, which are
     those vertices whose incident faces' orientation differences are
     aproximated 90°. The argument `pd' is a vtkPolydata, it have to be its
-    faces orientation (normal) calculated before.
+    faces orientation (normal) calculated before and make sure that
+    BuildLinks() has been called.
     """
-    pass
+    output = []
+    nv = pd.GetNumberOfPoints() # Number of vertices.
+    scalars = vtk.vtkIntArray()
+    for vid in xrange(nv):
+        idfaces = vtk.vtkIdList()
+        pd.GetPointCells(vid, idfaces) # Getting faces connected to face vid.
+
+        nf = idfaces.GetNumberOfIds()
+        orientations = []
+        # For each combination of connected faces
+        for nid in xrange(nf):
+            fid = idfaces.GetId(nid)
+            ni = pd.GetCellData().GetArray("Normals").GetTuple(fid)
+
+            of = 1 - np.dot(ni, stack_orientation)
+            orientations.append(of)
+
+        minimun = min(orientations)
+        maximun = max(orientations)
+
+        # Getting the ones which normals dot is 90°, its vertex is added to
+        # output
+        if maximun - minimun >= T:
+            output.append(vid)
+            scalars.InsertNextValue(1)
+            print "aqui"
+        else:
+            scalars.InsertNextValue(0)
+
+
+    pd.GetPointData().SetScalars(scalars)
+
+    return output
+
 
 def read_stl(filename):
     "It only reads a STL file, it can be binary or ascii format."
@@ -20,11 +56,48 @@ def read_stl(filename):
     stl_reader.SetFileName(filename)
     stl_reader.Update()
 
+
     output = stl_reader.GetOutput()
     return output
 
+def visualize(pd, vertices_staircase):
+    lt = vtk.vtkLookupTable()
+    lt.SetNumberOfColors(2)
+    lt.Build()
+    lt.SetTableValue(0, 1, 1, 1, 1)
+    lt.SetTableValue(1, 1, 0, 0, 1)
+
+    m = vtk.vtkPolyDataMapper()
+    m.SetInput(pd)
+    m.SetScalarRange(0, 1)
+    m.SetLookupTable(lt)
+
+    a = vtk.vtkActor()
+    a.SetMapper(m)
+
+    ren = vtk.vtkRenderer()
+    ren.AddActor(a)
+
+    renwin = vtk.vtkRenderWindow()
+    renwin.AddRenderer(ren)
+
+    iren = vtk.vtkRenderWindowInteractor()
+    iren.SetRenderWindow(renwin)
+    iren.Start()
+
+
 def main():
-    pd = read_stl(sys.argv[1])
+    stl = read_stl(sys.argv[1])
+
+    normals = vtk.vtkPolyDataNormals()
+    normals.SetInput(stl)
+    normals.ComputeCellNormalsOn()
+    normals.Update()
+    
+    pd = normals.GetOutput()
+    pd.BuildLinks()
+    vertices_staircase = find_staircase_artifacts(pd)
+    visualize(pd, vertices_staircase)
 
 
 if __name__ == '__main__':
