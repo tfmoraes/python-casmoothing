82cf4be33ab6e903a47efaeafc318bd4154100b0
Thiago Franco de Moraes <totonixsame@gmail.com>
2012-02-08 11:16:40 -0200

    Using an array instead of a vector in taubin_smooth. Removed the python code which were commented.

--- ca_smoothing.cxx
+++ ca_smoothing.cxx
@@ -26,6 +26,7 @@
 #include <vtkProperty.h>
 #include <vtkVertexGlyphFilter.h>
 #include <vtkSTLReader.h>
+#include <vtkPLYWriter.h>
 #include <vtkPolyDataNormals.h>
 #include <vtkCleanPolyData.h>
 #include <vtkDataArray.h>
@@ -73,12 +74,21 @@ int main(int argc, char *argv[])
 
     vertices_staircase = find_staircase_artifacts(pd, stack_orientation, atof(argv[2]));
     weights = calc_artifacts_weight(pd, vertices_staircase, atof(argv[3]), atof(argv[4]));
-    tpd = taubin_smooth(pd, weights, 0.5, -0.53, 10);
+    tpd = taubin_smooth(pd, weights, 0.5, -0.53, atoi(argv[5]));
+
+    vertices_staircase->Delete();
+    weights->Delete();
     
     vtkXMLPolyDataWriter *writer = vtkXMLPolyDataWriter::New();
     writer->SetInput(tpd);
     writer->SetFileName("saida.vtp");
     writer->Write();
+    
+    vtkPLYWriter *stl_writer = vtkPLYWriter::New();
+    stl_writer->SetInput(tpd);
+    stl_writer->SetFileName(argv[6]);
+    stl_writer->Write();
+
     return 0;
 }
 
@@ -231,6 +241,7 @@ vtkDoubleArray* calc_artifacts_weight(vtkPolyData* pd, vtkIdList* vertices_stair
                 scalars->SetTuple1(vjid, value);
             }
         }
+        near_vertices->Delete();
     }
 
     vtkPointData* pointData = pd->GetPointData();
@@ -267,7 +278,7 @@ Point calc_d(vtkPolyData* pd, int vid){
         D.z = D.z + (vi[2] - vj[2]);
         n++;
     }
-    
+
     D.x = D.x / n;
     D.y = D.y / n;
     D.z = D.z / n;
@@ -278,8 +289,10 @@ vtkPolyData* taubin_smooth(vtkPolyData* pd, vtkDoubleArray* weights, double l, d
     double vi[3];
     vtkPolyData* new_pd = vtkPolyData::New();
     new_pd->DeepCopy(pd);
-    std::vector<Point> D(pd->GetNumberOfPoints());
+    //std::vector<Point> D(pd->GetNumberOfPoints());
     //D.reserve(pd->GetNumberOfPoints());
+    Point *D;
+    D = (Point*) malloc(pd->GetNumberOfPoints() * sizeof(Point));
     
     for (int s=0; s < steps; s++) {
         for (int i=0; i < pd->GetNumberOfPoints(); i++) {
@@ -306,200 +319,6 @@ vtkPolyData* taubin_smooth(vtkPolyData* pd, vtkDoubleArray* weights, double l, d
             new_pd->GetPoints()->SetPoint(i, vi);
         }
     }
+    free(D);
     return new_pd;
 }
-
-/*
-def get_near_vertices_to_v(pd, v, dmax):
-    """
-    Returns all vertices with distance at most "d" to the vertice "v" with
-    their distances.
-    pd - vtkPolydata
-    v - the reference vertice
-    dmax - the maximun distance.
-    """
-    visit = []
-    vi = pd.GetPoint(v) # The position of vertice v
-    n = 0
-    while 1:
-        idfaces = vtk.vtkIdList()
-        pd.GetPointCells(v, idfaces) # Getting faces connected to face v
-        nf = idfaces.GetNumberOfIds()
-        # For each combination of connected faces
-        for nid in xrange(nf):
-            fid = idfaces.GetId(nid)
-            face = pd.GetCell(fid)
-            
-            for i in xrange(3):
-                vjid = face.GetPointId(i)
-                if vjid not in visit:
-                    vj = pd.GetPoint(vjid)
-                    d = np.sqrt((vi[0]-vj[0])**2 + (vi[1]-vj[1])**2 + (vi[2]-vj[2])**2)
-                    if d <= dmax:
-                        yield vjid, d
-                        visit.append(vjid)
-        n += 1
-        print n
-
-        if n >= len(visit):
-            break
-
-        v = visit[n]
-
-def calc_artifacts_weight(pd, vertices_staircase, tmax=5.0, bmin=1):
-    """
-    Calculate the artifact weight based on distance of each vertex to its
-    nearest staircase artifact vertex.
-    pd - vtkPolydata;
-    vertices_staircase - the identified staircase artifact vertices;
-    tmax=2 - max distance the vertex must be to its nearest artifact vertex to
-             considered to calculate the weight;
-    bmin=0.1 - The minimun weight.
-    """
-    weights = np.zeros(pd.GetNumberOfPoints(), dtype='float64')
-    scalars = pd.GetPointData().GetScalars()
-    for vi in vertices_staircase:
-        for vj, d in get_near_vertices_to_v(pd, vi, tmax):
-            value = (1.0 - d/tmax)
-            if value > weights[vj]:
-                weights[vj] = value
-                scalars.SetValue(vj, value)
-    weights = weights * (1 - bmin) + bmin
-    return weights
-
-def calculate_d(mesh, poly, pid):
-    t = 0
-    n = 0.0
-    cell_ids = vtk.vtkIdList()
-    p0 = np.array(poly.GetPoint(pid))
-    mesh.GetPointCells(pid, cell_ids)
-    for i in xrange(cell_ids.GetNumberOfIds()):
-        point_ids = vtk.vtkIdList()
-        mesh.GetCellPoints(cell_ids.GetId(i), point_ids)
-        n += 1
-        if point_ids.GetId(0) != pid:
-            p1 = np.array(poly.GetPoint(point_ids.GetId(0)))
-        else:
-            p1 = np.array(poly.GetPoint(point_ids.GetId(1)))
-
-        t = t + (p1 - p0)
-
-    return t / n
-
-def taubin_smooth(pd, weights, l, m, steps):
-    edgesfilter = vtk.vtkExtractEdges()
-    edgesfilter.SetInput(pd)
-    edgesfilter.Update()
-
-    edges = edgesfilter.GetOutput()
-
-    new_pd = vtk.vtkPolyData()
-    new_pd.DeepCopy(pd)
-
-    points = new_pd.GetPoints()
-    for s in xrange(steps):
-        D = {}
-        for i in xrange(edges.GetNumberOfPoints()):
-            D[i] = calculate_d(edges, new_pd, i)
-        for i in xrange(pd.GetNumberOfPoints()):
-            p = np.array(points.GetPoint(i))
-            pl = p + weights[i]*l*D[i]
-            nx, ny, nz = pl
-            points.SetPoint(i, nx, ny, nz)
-
-        #D = {}
-        #for i in xrange(edges.GetNumberOfPoints()):
-            #D[i] = calculate_d(edges, new_pd, i)
-        #for i in xrange(pd.GetNumberOfPoints()):
-            #p = np.array(points.GetPoint(i))
-            #pl = p + weights[i]*m*D[i]
-            #nx, ny, nz = pl
-            #points.SetPoint(i, nx, ny, nz)
-
-        #D = {}
-        #for i in xrange(edges.GetNumberOfPoints()):
-            #D[i] = calculate_d(edges, i)
-        #for i in xrange(pd.GetNumberOfPoints()):
-            #x, y, z = points.GetPoint(i)
-            #nx = x + m*D[i][0]
-            #ny = y + m*D[i][1]
-            #nz = z + m*D[i][2]
-            #points.SetPoint(i, nx, ny, nz)
-
-    new_pd.SetPoints(points)
-    return new_pd
-
-
-def read_stl(filename):
-    "It only reads a STL file, it can be binary or ascii format."
-
-    stl_reader = vtk.vtkSTLReader()
-    stl_reader.SetFileName(filename)
-    stl_reader.Update()
-
-    output = stl_reader.GetOutput()
-    return output
-
-
-def visualize(pd, vertices_staircase):
-    lt = vtk.vtkLookupTable()
-    lt.SetNumberOfColors(100)
-    lt.SetValueRange(0, 1)
-    #lt.Build()
-    lt.SetTableValue(0, 0, 0, 1, 1)
-    lt.SetTableValue(99, 1, 0, 0, 1)
-
-    m = vtk.vtkPolyDataMapper()
-    m.SetInput(pd)
-    m.SetScalarRange(0.0, 1.0)
-    m.SetLookupTable(lt)
-
-    a = vtk.vtkActor()
-    a.SetMapper(m)
-
-    ren = vtk.vtkRenderer()
-    ren.AddActor(a)
-
-    renwin = vtk.vtkRenderWindow()
-    renwin.AddRenderer(ren)
-
-    iren = vtk.vtkRenderWindowInteractor()
-    iren.SetRenderWindow(renwin)
-    iren.Start()
-
-
-def main():
-    parser = argparse.ArgumentParser(prog='Context-aware mesh smoothing')
-    parser.add_argument("file", help="A STL file", metavar="STL file")
-    parser.add_argument("-t", "--threshold", help="Threshold to find staircase artifacts", 
-                       type=float, default=0.7, dest='threshold')
-    args = parser.parse_args()
-
-    stl = read_stl(args.file)
-
-    normals = vtk.vtkPolyDataNormals()
-    normals.SetInput(stl)
-    normals.ComputeCellNormalsOn()
-    normals.Update()
-
-    clean = vtk.vtkCleanPolyData()
-    clean.SetInput(normals.GetOutput())
-    clean.Update()
-    
-    pd = clean.GetOutput()
-    pd.BuildLinks()
-    vertices_staircase = find_staircase_artifacts(pd, T=args.threshold)
-    weights = calc_artifacts_weight(pd, vertices_staircase)
-    print weights
-    new_pd = taubin_smooth(pd, weights, 0.5, -0.53, 10)
-    visualize(new_pd, vertices_staircase)
-
-    writer = vtk.vtkXMLPolyDataWriter()
-    writer.SetInput(new_pd)
-    writer.SetFileName("/tmp/test_cor.vtp")
-    writer.Write()
-
-
-if __name__ == '__main__':
-    main()
-*/
